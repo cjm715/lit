f1adfb1db49c64d768fc462c7cd2296f5f0fe185
cjm715 <cjmiles2006@gmail.com>
2017-10-04 16:28:44 -0400

    adding new pyton files

---
+++ run_simulations_on_flux.py
@@ -0,0 +1,12 @@
+import fluxtools
+from simulations import lit_enstrophy_sim
+
+if __name__ == "__main__":
+    python_file = 'lit_enstrophy.py'
+    flux_folder_name = 'lit_enstrophy/'
+    dict_of_parameter_lists = {'Pe': [8.0, 16.0, 32.0, 64.0, 128.0, 256.0,
+                                      512.0, 1024.0, 2048.0, 4096.0, 8192.0, 16384.0],
+                               'N': [64, 128, 256, 512]}
+
+    fluxtools.run_python_file(
+        python_file, dict_of_parameter_lists, flux_folder_name)
